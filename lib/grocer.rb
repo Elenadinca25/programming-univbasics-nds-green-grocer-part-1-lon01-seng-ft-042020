0da2a6c13148da0d2ce8d017b65f0606def8062d
Elena Dinca <elenadinca25@gmail.com>
2020-04-15 12:33:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,6 @@
 def find_item_by_name_in_collection(name, collection)
 counter = 0
-while counter < collection.lenght
+while counter < collection.length
   if collection[counter][:item] == name
     return collection[counter]
     end
