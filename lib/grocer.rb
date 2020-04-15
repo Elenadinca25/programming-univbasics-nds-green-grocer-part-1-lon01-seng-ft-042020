e911809f0f03d822f3e5656989eecba94587d307
Elena Dinca <elenadinca25@gmail.com>
2020-04-15 13:21:31 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,13 +19,13 @@ def consolidate_cart(cart)
        new_cart_item[:count] += 1
 
   else
-    new_cart_item {
-      :item => cart[counter][:item]
-      :price => cart[counter][:price]
-      :clearance=> cart[counter][:clearance]
+    new_cart_item = {
+      :item => cart[counter][:item],
+      :price => cart[counter][:price],
+      :clearance=> cart[counter][:clearance],
       :count => 1
-    }
-    new_cart << new_cart_item
+      }
+      new_cart << new_cart_item
       end
     counter += 1
     end
